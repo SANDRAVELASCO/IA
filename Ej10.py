0a595fe02816ff201c9af4562688798c3a05d2b8
SANDRAVELASCO <itakuaa0011@gmail.com>
2025-02-13 07:59:32 -0600

    Subiendo el ejercicio 10

---
+++ Ej10.py
@@ -0,0 +1,8 @@
+# Pedimos al usuario que ingrese un número
+numero = int(input("Ingresa un número: "))
+
+# Verificamos si el número es mayor que 10
+if numero > 10:
+    print(f"{numero} es mayor que 10.")
+else:
+    print(f"{numero} no es mayor que 10.")
